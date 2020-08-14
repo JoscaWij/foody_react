81b68057f668d881ab6f96b7236ee8edb2e1a3d5
Josca Wijtenburg <68224111+JoscaWij@users.noreply.github.com>
2020-08-14 11:42:17 +0200

    Basic layout (#2)
    * Add basic HTML-layout in JSX
    
    * Fix footer text
    
    * Add basic grid layout and flat-bg-colors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app fullpage">
+      <header className="headerSearch">
+        <h1 className="appTitle">Foody</h1>
+        <span>Foody wie bei Mutti</span>
+        <input placeholder="Search for recipe"></input>
+      </header>
+      <main className="mealList"></main>
+      <footer className="footer-copyright">
+        <small>© mealAPI & JW</small>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
