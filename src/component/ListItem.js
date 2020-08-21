e0a96e3bf4d230925c58f349de2cf2dea463e3ed
JoscaWij <Josca.W@gmx.net>
2020-08-21 11:32:07 +0200

    Refacture ListItem

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./ListItem.css";
 import ExpandableRecipe from "./ExpandableRecipe";
+import styled from "@emotion/styled";
 
 function ListItem({ title, imgSrc, instructions }) {
   const [open, setOpen] = React.useState(false);
@@ -9,7 +9,7 @@ function ListItem({ title, imgSrc, instructions }) {
   };
 
   return (
-    <div className="listItem">
+    <ListItemStyled>
       <img className="listItem_img" src={imgSrc} alt={title} />
       <span className="listItem_title">{title}</span>
       <button className="listItem_button" onClick={handleClick}>
@@ -18,8 +18,48 @@ function ListItem({ title, imgSrc, instructions }) {
       {open && (
         <ExpandableRecipe mealTitle={title} instructions={instructions} />
       )}
-    </div>
+    </ListItemStyled>
   );
 }
 
 export default ListItem;
+
+const ListItemStyled = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: solid var(--bg-color) 1px;
+  padding: 0.5em 0;
+
+  *:last-child {
+    border: none;
+  }
+
+  img {
+    height: 6em;
+    width: 6em;
+    object-fit: contain;
+    border-radius: 15%;
+  }
+
+  span {
+    font-size: 2em;
+    font-family: "Covered By Your Grace", cursive;
+  }
+
+  button {
+    border-style: none;
+    border-radius: 20px;
+    height: 3em;
+    padding: 0 1em;
+    background-color: var(--bg-color);
+    transition: 0.3s;
+    color: white;
+    font-weight: bolder;
+  }
+
+  button:hover {
+    background-color: var(--bg-color-dark);
+  }
+`;
