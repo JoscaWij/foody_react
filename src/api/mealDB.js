7b86c064d2c9eed061d6ce6ed2ad96093524d17c
JoscaWij <Josca.W@gmx.net>
2020-08-24 08:56:22 +0200

    Add Query-Hook

--- src/api/mealDB.js
+++ src/api/mealDB.js
@@ -1,4 +1,4 @@
-export async function fetchRandomRecipes() {
+/* export async function fetchRandomRecipes() {
   const response = await fetch(
     "https://www.themealdb.com/api/json/v1/1/random.php"
   );
@@ -18,5 +18,42 @@ export async function fetchRandomRecipes() {
     imgSrc: meal.strMealThumb,
   }));
 
-  return result;
+  console.log(result[0]);
+
+  return result; 
+}
+*/
+
+export async function fetchRandomRecipes() {
+  const randomRecipes = [];
+  while (randomRecipes.length < 10) {
+    const response = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    const data = await response.json();
+
+    randomRecipes.push(data.meals[0]);
+  }
+  const recipes = randomRecipes.map((meal) => ({
+    id: meal.idMeal,
+    title: meal.strMeal,
+    category: meal.strCategory,
+    area: meal.strArea,
+    instructions: meal.strInstructions,
+    imgSrc: meal.strMealThumb,
+  }));
+
+  /*   const recipes = recipeData.filter(
+    (recipe, index) =>
+      recipeData.findIndex((other) => other.id === recipe.id) === index
+  ); */
+
+  console.log(recipes);
+
+  return recipes;
 }
--- src/pages/MainRecipeList.js
+++ src/pages/MainRecipeList.js
@@ -2,28 +2,36 @@ import React from "react";
 import List from "../component/List";
 import ListItem from "../component/ListItem";
 import ExpandableRecipe from "../component/ExpandableRecipe";
-import useFetch from "../hooks/useFetch";
+/* import useFetch from "../hooks/useFetch"; */
 import { fetchRandomRecipes } from "../api/mealDB";
+import { useQuery } from "react-query";
 
 function MainRecipeList() {
-  const { recipes, loading, error } = useFetch(fetchRandomRecipes, 10);
+  /* const { recipes, loading, error } = useFetch(fetchRandomRecipes, 10); */
+  const { data: recipes, status } = useQuery("recipes", fetchRandomRecipes);
+  console.log(status);
+  console.log(recipes);
 
   return (
     <main>
-      {loading && <div>Calling my Mum to ask ...</div>}
-      {error && <div>Error! My Mum had no answer. Please try again later</div>}
-      <List>
-        {recipes?.map((recipe) => (
-          <ListItem
-            key={recipe.id}
-            title={recipe.title}
-            imgSrc={recipe.imgSrc}
-            instructions={recipe.instructions}
-          >
-            <ExpandableRecipe />
-          </ListItem>
-        ))}
-      </List>
+      {status === "loading" && <div>Calling my Mum to ask ...</div>}
+      {status === "error" && (
+        <div>Error! My Mum had no answer. Please try again later</div>
+      )}
+      {status === "success" && (
+        <List>
+          {recipes?.map((recipe) => (
+            <ListItem
+              key={recipe.id}
+              title={recipe.title}
+              imgSrc={recipe.imgSrc}
+              instructions={recipe.instructions}
+            >
+              <ExpandableRecipe />
+            </ListItem>
+          ))}
+        </List>
+      )}
     </main>
   );
 }
